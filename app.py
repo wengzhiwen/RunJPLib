6cab38500adb9c7c761ea0c742dae5712a06f40d
James Weng <wengzhiwen@gmail.com>
2025-03-29 04:25:43 +0000

    更新环境配置文件，添加FLASK_APP_PORT变量并修改应用启动端口为环境变量值

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ if __name__ == '__main__':
     logging.debug("日志配置完成，准备启动应用")
     
     # 启动应用
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('FLASK_APP_PORT', 5000))
